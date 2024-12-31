db026fd3fdd9e533227c271d0a2f7dc3b727c45f
Mawira <brian.mawi@yahoo.com>
2024-12-31 13:01:39 +0300

    add necessary pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/Home/Home.vue";
 import Login from "../views/Auth/Login.vue";
 import Register from "../views/Auth/Register.vue";
 import ForgotPassword from "../views/Auth/ForgotPassword.vue";
+import NotFound from "../views/404.vue";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: "/auth/forgot-password",
     name: "forgot password",
     component: ForgotPassword,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "404",
+    component: NotFound,
   },
 ];
 
